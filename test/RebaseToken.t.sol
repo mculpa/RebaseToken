3a06aa94ee3b09e4d91efc38160cd45b187f04d7
Mak <antonenko.makar123@gmail.com>
2025-08-04 22:17:16 -0400

    RebaseToken.t first steps

---
+++ test/RebaseToken.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {Vault} from "../src/Vault.sol";
+
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    address public owner = makeAddr("owner");
+    address public user = makeAddr("user");
+
+    function setUp() public {
+        vm.startPrank(owner);
+        rebaseToken = new RebaseToken();
+        vault = new Vault(IRebaseToken(address(rebaseToken)));
+        rebaseToken.grantMintAndBurnRole(address(vault));
+        (bool success,) = payable(address(vault)).call{value: 1e18}("");
+
+        vm.stopPrank();
+    }
+
+    function testDepositLinear(uint256 amount) public {
+        vm.assume(amount > 1e5);
+        amount = bound(amount, 1e5, type(uint96).max);
+
+        vm.startPrank(user);
+        vm.deal(user, amount);
+
+
+        vm.stopPrank();
+    }
+}
