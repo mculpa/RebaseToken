5f591ee74ed5da2ed2bc9b9fe3c54c68d5c8bb95
Mak <antonenko.makar123@gmail.com>
2025-08-07 16:28:10 -0400

    RebaseToken.t fix

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -15,9 +15,9 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
-    uint256 private constant PRECISION_FACTOR = 1e20;
+    uint256 private constant PRECISION_FACTOR = 1e18;
     bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
-    uint256 private s_interestRate = (5 * PRECISION_FACTOR) / 1e8;
+    uint256 private s_interestRate = 5e10;
     mapping(address => uint256) private s_userInterestRate;
     mapping(address => uint256) s_lastUpdatedTimeStamp;
 
@@ -30,6 +30,7 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     }
 
     function setInterestRate(uint256 _newInterestRate) external onlyOwner {
+        // Set the interest rate
         if (_newInterestRate >= s_interestRate) {
             revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
@@ -94,15 +95,16 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     }
 
     function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
-        _mintAccruedInterest(_sender);
-        _mintAccruedInterest(_recipient);
         if (_amount == type(uint256).max) {
             _amount = balanceOf(_sender);
         }
+        // accumulates the balance of the user so it is up to date with any interest accumulated.
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
         if (balanceOf(_recipient) == 0) {
+            // Update the users interest rate only if they have not yet got one (or they tranferred/burned all their tokens). Otherwise people could force others to have lower interest.
             s_userInterestRate[_recipient] = s_userInterestRate[_sender];
         }
-
         return super.transferFrom(_sender, _recipient, _amount);
     }
 
